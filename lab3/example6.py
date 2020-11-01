7f135167cd9ca1c9bcc7f4021420024b8bf64844
r35934594 <ecesavran@std.iyte.edu.tr>
2020-11-01 19:15:23 +0000

    .

--- lab3/example6.py
+++ lab3/example6.py
@@ -7,15 +7,15 @@ discriminant=(b**2)+(-4*a*c)
 if discriminant>0:
   x_1=(-b+(discriminant)**(1/2))/(2*a)
   x_2=(-b-(discriminant)**(1/2))/(2*a)
-  print("First x value is "+str(x_1)+"and the second value is "+str(x_2))
+  print("First x value is ", x_1 ,"and the second value is ",x_2)
 elif discriminant==0:
   x=-b/(2*a)
-  print("x is "+str(x))
+  print("x is ",x)
 else:
   x_1=-b/(2*a)
   i_1=((-discriminant)**(1/2))/(2*a)
   x_2=-b/(2*a)
   i_2=-((-discriminant)**(1/2))/(2*a)
-  print("First x value is "+str(x_1)+"+"+str(i_1)
-+"i"+"and the second value is "+str(x_2)+ "+" +str(i_2)+"i")
+  print("First x value is ",x_1,"+",i_1
+,"i","and the second value is ",x_2, "+",i_2,"i")
   
