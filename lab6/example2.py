b8651231cb7c00ee1a8f597c93d497c72850c828
r35934594 <ecesavran@std.iyte.edu.tr>
2020-11-23 09:49:18 +0000

    example2 done

---
+++ lab6/example2.py
@@ -0,0 +1,19 @@
+students=int(input("How many students are there?"))
+grades=[]
+final_grades=[]
+for i in range(students):
+  midterm1=int(input("Please enter the student's first midter:"))
+    
+  midterm2=int(input("Please enter the student's second midter:"))
+  
+  final=int(input("Please enter the student's final:"))
+   
+  weigthed_grade=midterm1*0.3+midterm2*0.3+final*0.4
+ 
+  grades.append([midterm1,midterm2,final])
+  final_grades.append([weigthed_grade])
+
+print(grades)
+print(final_grades)
+
+
