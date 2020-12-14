aab62916736906a6e1bb94e75453b5e933d53f2e
r35934594 <ecesavran@std.iyte.edu.tr>
2020-12-14 09:00:47 +0000

    .

---
+++ lab8/example2.py
@@ -0,0 +1,17 @@
+def is_prime(num):
+  for i in range(2,num):
+    if num%i==0:
+      return "not prime"
+  return "prime"
+
+def print_primes_between(x,y):
+  for num in range(x,y+1):
+    if num>1:
+      if is_prime(num)=="prime":
+        print(num)
+
+x=int(input("Enter an integer:"))
+y=int(input("Enter an integer:"))
+print(is_prime(x))
+print(is_prime(y))
+print_primes_between(x,y)
